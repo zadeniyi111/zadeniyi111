e3683f64c32ceefdc9665541be3cc83bee0016c5
zadeniyi111 <84995078+zadeniyi111@users.noreply.github.com>
2021-05-28 22:25:10 +0100

    Add files via upload

---
+++ Adeniyi.py
@@ -0,0 +1,92 @@
+USSD_CODE = ("ACCESS BANK : *901#")
+print(USSD_CODE)
+display = "Please type in your banks ussd code: 901 "
+print(display)
+display_0= "You have entered the code for access bank of Nigeria"
+print(display_0)
+list = [ "1.Check your balance",  "2.Transfer",  "3.Airtime", "4.other services", "5.Access money","6.Daimond xtra", "7.Xtracash loan", "8.mobile wallet"]
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+option_1='please select an option: 1'
+print(option_1)
+display_1= 'Your Acct 0810350768 On 03 Dec 2020 15:12:41 Bal: NGN18009.81CR'
+print (display_1)
+option_0=   "ENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "ENTER 2 TO END"
+print(option_05)
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+option_2 = 'Please select an option: 2'
+print (option_2)
+display_7= 'Enter account number you want to transfer money to: 0810350768 '
+print(display_7)
+display_6='Enter Amount : 2000'
+print(display_6)
+display_3= "Enter pin :5050"
+print(display_3)
+display_4= "Do you want to send 2000 to 0810350768?"
+print(display_4)
+option_3= ["1.YES" , "2.NO"]
+print(option_3[0])
+print(option_3[1])
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+display_4 = "TRANSACTION SUCCESSFUL"
+print(display_4)
+option_0=   "ENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "ENTER 2 TO END"
+print(option_05)
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+option_3= "Please select an option: 3"
+print(option_3)
+list_1=[ "1.For self" , "2.For others" ]
+print( list_1[0])
+print ( list_1[1])
+display_4 = "Enter 1 or 2 : 2"
+print(display_4)
+display_6= 'Enter Amount : 2000'
+print(display_6)
+display_7= 'Enter  number you want to buy airtime for: 08130376006'
+print(display_7)
+display_4= "Do you want to buy 2000 naira worth of airtime for 08130376006?"
+print(display_4)
+option_3= ["1.YES" , "2.NO"]
+print(option_3[0])
+print(option_3[1])
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+display_4 = "TRANSACTION SUCCESSFUL"
+print(display_4)
+option_0=   "ENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "ENTER 2 TO END"
+print(option_05) 
+display_4 = "Enter 1 or 2 : 2"
+print(display_4)
+display_11="An SMS message will be sent to you shortly."
+print(display_11)
